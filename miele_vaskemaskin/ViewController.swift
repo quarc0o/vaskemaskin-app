17b0c0b9c1ccf68978274616ff67515a9685eaba
quarc0o <emmanuelq150@gmail.com>
2022-09-04 03:12:40 +0200

    Initial Commit

---
+++ miele_vaskemaskin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miele_vaskemaskin
+//
+//  Created by Emmanuel Quarcoo on 04/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
